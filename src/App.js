d2e8bd0b587221171ce9fa2c96cd17e0ab42b3c4
Devanshu Kumar <devanshukumar289@gmail.com>
2024-01-05 21:06:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Homepage from "./components/Homepage/Homepage";
 import Admin from "./components/Admin/Admin";
 import Employee from "./components/Employee/Employee";
